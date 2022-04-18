cafe3b1631af13c5d6c8538734802bdc7a26d8cb
Alim985 <95762276+Alim985@users.noreply.github.com>
2022-04-18 09:57:47 -0700

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,38 @@
+(function($){
+
+$('.slaider-area1').slick({
+	slidesToShow:3  ,
+	arrows: false,
+	dots: true,
+
+});
+
+
+})(jQuery);
+
+(function($){
+
+$('.slaider-area144').slick({
+	slidesToShow:0,
+	arrows: true,
+	dots: true,
+
+});
+
+
+})(jQuery);
+
+(function($){
+
+$('.ltn__img-slide-item-').slick({
+	slidesToShow: 4,
+	arrows: true,
+	dots: true,
+
+});
+
+
+})(jQuery);
+
+
+
